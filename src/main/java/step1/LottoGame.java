6c5a1ac7337c427f2dda6bdc2228f5f7a249b50a
eojinkim1 <rladjwls999@gmail.com>
2024-04-22 01:44:48 +0900

    로또게임 구현 실행 부분 수정

--- src/main/java/step1/LottoGame.java
+++ src/main/java/step1/LottoGame.java
@@ -1,75 +1,16 @@
 package step1;
 
-import java.util.Arrays;
-import java.util.InputMismatchException;
-import java.util.Random;
-import java.util.Scanner;
-
 public class LottoGame {
-    private static final Scanner SCANNER = new Scanner(System.in);
-    private static final Random RANDOM = new Random();
-    private static final int inputLength = 6;
-    private static String[] INPUT_STR_WINNING_NUMBER = new String[6];
-
     public static void main(String[] args) {
         PurchaseAmount purchaseAmount = new PurchaseAmount();
-        int randomLottoNumber[] = new int[6];
+        WinningNumber winningNumber = new WinningNumber();
 
-        int quantity = purchaseAmount.buyLotto();
+        purchaseAmount.myLotto();
 
-        System.out.println(quantity + "개를 구매했습니다.");
-        for (int i = 0; i < quantity; i++) {
-            for (int j = 0; j < randomLottoNumber.length; j++) {
-                int lottoNumber = RANDOM.nextInt(45) + 1;
-                randomLottoNumber[j] = lottoNumber;
-                for (int k = 0; k < j; k++) {
-                    if (randomLottoNumber[j] == randomLottoNumber[k]) {
-                        j--;
-                        break;
-                    }
-                }
-            }
-            System.out.println(Arrays.toString(randomLottoNumber));
+        while (true){
+            winningNumber.numberConfirm();
+            break;
         }
-//
-//        while (true) {
-//            System.out.println("당첨 번호를 입력해 주세요");
-//            while (true) {
-//                try {
-//                    String inputWinningNumber = SCANNER.next();
-//
-//                    INPUT_STR_WINNING_NUMBER = inputWinningNumber.split(",");
-//                    if (inputWinningNumber.length() != inputLength) {
-//                        System.out.println("(,)로 구분하여 6개의 값을 입력해주세요.");
-//                        continue;
-//                    }
-//                    boolean isValid = true;
-//                    for (String num : INPUT_STR_WINNING_NUMBER) {
-//                        int value = Integer.parseInt(num.trim());
-//                        if (value < 1 || value > 45) {
-//                            System.out.println("1~45 사이의 숫자를 (,)로 구분하여 6개의 값을 입력해주세요.");
-//                            isValid = false;
-//                            break;
-//                        }
-//                    }
-//                    if (!isValid) {
-//                        continue;
-//                    }
-//                    return;
-//                } catch (InputMismatchException | NumberFormatException e) {
-//                    System.out.println("(,)로 구분하여 6개의 값을 입력해주세요.");
-//                }
-//            }
-////            int[] winningNumber = new int[inputLength];
-////            for (int i = 0; i < inputLength; i++) {
-////                winningNumber[i] = Integer.parseInt(INPUT_STR_WINNING_NUMBER[i].trim());
-////            }
-//
-//        }
-//
-////        System.out.println("보너스 번호를 입력해 주세요");
-////        SCANNER.nextInt();
-//
 ////        일치 조건 문
 ////        for (int i = 0; i < quantity; i++) {
 ////            for (int j = 0; j < 6; j++) {
