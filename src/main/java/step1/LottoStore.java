cf59be224372d44541ee5a5bdd871c1ca9ac0d10
eojinkim1 <rladjwls999@gmail.com>
2024-04-24 04:14:47 +0900

    로또 판매 클래스 생성 완료

---
+++ src/main/java/step1/LottoStore.java
@@ -0,0 +1,20 @@
+package step1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoStore {
+    private static final int PRICE = 1_000;
+    public List<Lotto> sellLotto(int money) {
+        if (money < PRICE) {
+            throw new IllegalArgumentException("1000원 이상의 값을 입력해주세요");
+        }
+        int quantity = money / PRICE;
+
+        List<Lotto> lottoList = new ArrayList<>();
+        for (int i = 0; i < quantity; i++) {
+            lottoList.add(Lotto.random());
+        }
+        return lottoList;
+    }
+}
