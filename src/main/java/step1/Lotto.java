d9f5e14d1ed736d634dbba440d5e6d626e28e5a3
eojinkim1 <rladjwls999@gmail.com>
2024-04-24 04:12:51 +0900

    로또가 중복값 없이 만들어지도록 수정

--- src/main/java/step1/Lotto.java
+++ src/main/java/step1/Lotto.java
@@ -15,7 +15,13 @@ public class Lotto {
         Random random = new Random();
         int[] numbers = new int[6];
         for (int i = 0; i < 6; i++) {
-            numbers[i] = random.nextInt(45) + 1;
+            numbers[i] = random.nextInt(MAXIMUM_NUMBER) + 1;
+            for (int j = 0; j < i; j++) {
+                if (numbers[i] == numbers[j]){
+                    i--;
+                    break;
+                }
+            }
         }
         return new Lotto(numbers);
     }
