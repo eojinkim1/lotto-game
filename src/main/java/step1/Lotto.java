52eb18ae42793984c9cbb309a5fca2deafc59461
eojinkim1 <rladjwls999@gmail.com>
2024-04-26 00:59:17 +0900

    로또 번호 샐성 클래스 변경

--- src/main/java/step1/Lotto.java
+++ src/main/java/step1/Lotto.java
@@ -1,49 +1,30 @@
 package step1;
 
-import java.util.Random;
+import java.util.*;
 
 public class Lotto {
-    private int[] numbers; //글로벌 변수,필드 변수, 인스턴트 변수, 속성값
-    private static final int MINIMUM_NUMBER = 1;
-    private static final int MAXIMUM_NUMBER = 45;
+    private Set<LottoNumber> numbers;//글로벌 변수,필드 변수, 인스턴트 변수, 속성값
 
-    public Lotto(int[] numbers) { // 생성자 new 로 밖에서 불릴 때 호출되는 거(로컬 변수) //함수는 초기화 용도 = 초기화는 변수에 어떤 값을 할당하는 것을 말함
-        validNumbers(numbers);
-        this.numbers = numbers; // this(글로벌 변수, 인스턴트 변수), 로컬에 있는 변수를 뜻함
+    public Lotto(Set<LottoNumber> numbers) { // 생성자 new 로 밖에서 불릴 때 호출되는 거(로컬 변수) //함수는 초기화 용도 = 초기화는 변수에 어떤 값을 할당하는 것을 말함
+        this.numbers = new HashSet<>(numbers); // this(글로벌 변수, 인스턴트 변수), 로컬에 있는 변수를 뜻함
     }
-    public static Lotto random(){
-        Random random = new Random();
-        int[] numbers = new int[6];
+
+    public static Lotto random() {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= 45; i++) {
+            numbers.add(i);
+        }
+        // 1 ~ 45
+        Collections.shuffle(numbers);
+        Set<LottoNumber> lottoNumbers = new HashSet<>();
         for (int i = 0; i < 6; i++) {
-            numbers[i] = random.nextInt(MAXIMUM_NUMBER) + 1;
-            for (int j = 0; j < i; j++) {
-                if (numbers[i] == numbers[j]){
-                    i--;
-                    break;
-                }
-            }
+            LottoNumber number = new LottoNumber(numbers.get(i));
+            lottoNumbers.add(number);
         }
-        return new Lotto(numbers);
+        return new Lotto(lottoNumbers);
     }
 
-    private void validNumbers(int[] numbers) {
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] < MINIMUM_NUMBER || numbers[i] > MAXIMUM_NUMBER){
-                throw new IllegalArgumentException("1~45범위 값을 초과 했습니다.");
-            }
-        }
-        for (int i = 0; i < numbers.length - 1; i++) {
-            for (int j = i + 1; j < numbers.length ; j++) {
-                if (numbers[i] == numbers[j]){
-                    throw new IllegalArgumentException("중복된 값입니다.");
-                }
-            }
-        }
-        if (numbers.length != 6){
-            throw new IllegalArgumentException("범위를 초과한 값입니다.");
-        }
-    }
-    public int[] getNumbers() {
-        return numbers;
+    public Set<LottoNumber> getNumbers() {
+        return new HashSet<>(numbers);
     }
 }
