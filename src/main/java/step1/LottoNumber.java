6e627c87901bd6a72d9f51b00ebb88470af25581
eojinkim1 <rladjwls999@gmail.com>
2024-04-26 22:57:44 +0900

    로또 번호 생성하는 클래스 생성

---
+++ src/main/java/step1/LottoNumber.java
@@ -0,0 +1,36 @@
+package step1;
+
+import java.util.Objects;
+
+public class LottoNumber {
+    private static final int MINIMUM_NUMBER = 1;
+    private static final int MAXIMUM_NUMBER = 45;
+    private int number;
+
+    public LottoNumber(int number) {
+        if (number < MINIMUM_NUMBER || number > MAXIMUM_NUMBER){
+            throw new IllegalArgumentException("숫자 생성 범위를 초과하였습니다.");
+        }
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber number1 = (LottoNumber) o;
+        return number == number1.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
+    @Override
+    public String toString() {
+        return "LottoNumber{" +
+                "number=" + number +
+                '}';
+    }
+}
