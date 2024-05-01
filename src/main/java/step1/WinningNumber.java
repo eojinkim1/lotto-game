30053d8a624d91e0c8ce53360e99978f80a46191
eojinkim1 <rladjwls999@gmail.com>
2024-05-02 00:00:06 +0900

    로또 등수 확인

--- src/main/java/step1/WinningNumber.java
+++ src/main/java/step1/WinningNumber.java
@@ -1,105 +1,38 @@
 package step1;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WinningNumber {
-    private static final Scanner SCANNER = new Scanner(System.in);
-    private static final int INPUT_LENGTH = 6;
-    private static String[] inputStrWinningNumber = new String[6];
-    private static int bonusNumber = 0;
 
-    public void numberConfirm() {
-        while (true) {
-            System.out.println("당첨 번호를 입력해 주세요");
-            try {
-                String inputWinningNumber = SCANNER.next();
-                inputStrWinningNumber = inputWinningNumber.split(",");
-
-                if (!isValidInput(inputStrWinningNumber)) {
-                    continue;
-                }
-                if (!isValidRange(inputStrWinningNumber)) {
-                    continue;
-                }
-                if (!deduplication(inputStrWinningNumber)) {
-                    continue;
-                }
-                break;
-            } catch (InputMismatchException | NumberFormatException | ArrayIndexOutOfBoundsException e) {
-                System.out.println("1~45 사이의 숫자를 (,)로 구분하여 6개의 값을 입력해주세요.#4");
-            }
-        }
-        int[] winningNumber = getWinningNumber();
-
-        bonusNumber = inputBonusNumber();
-        while (deduplication(winningNumber, bonusNumber)){
-            System.out.println("보너스 번호가 당첨 번호와 중복됩니다. 다시 입력해주세요.");
-            bonusNumber = inputBonusNumber();
-        }
+    private Lotto winningNumber;
+    private LottoNumber bonusNumber;
+    public WinningNumber(Lotto winningNumber, LottoNumber bonusNumber) {
+        this.winningNumber = winningNumber;
+        this.bonusNumber = bonusNumber;
     }
+    public int rank(Lotto lotto){
+        List<LottoNumber> lottoNumbers = new ArrayList<>(lotto.getNumbers());
+        List<LottoNumber> winningNumbers = new ArrayList<>(winningNumber.getNumbers());
 
-    public int[] getWinningNumber(){
-        int[] winningNumbers = new int[INPUT_LENGTH];
-        for (int i = 0; i < INPUT_LENGTH; i++) {
-            winningNumbers[i] = Integer.parseInt(inputStrWinningNumber[i].trim());
-        }
-        return winningNumbers;
-    }
-    public int getBonusNumber(){
-        return bonusNumber;
-    }
-    private static boolean deduplication(int[] winningNumbers, int bonusNumber){
-        for (int number : winningNumbers) {
-            if (number == bonusNumber){
-                return true;
-            }
-        }
-        return false;
-    }
+        long sameCount = lottoNumbers.stream() //시퀀스
+                .filter(winningNumbers::contains)
+                .count();
 
-    private static int inputBonusNumber() {
-        while (true){
-            System.out.println("보너스 번호를 입력해 주세요.");
-            int inputBonusNumber = SCANNER.nextInt();
-            if (inputBonusNumber < 1 || inputBonusNumber > 45){
-                System.out.println("1~45 사이의 숫자를 1개 입력해주세요.");
-                continue;
-            }
-            return inputBonusNumber;
-        }
-    }
+        boolean isSameBonusNumber = lottoNumbers.contains(bonusNumber);
 
-    private static boolean deduplication(String[] inputNumbers) {
-        for (int i = 0; i < INPUT_LENGTH - 1; i++) {
-            for (int j = i + 1; j < INPUT_LENGTH; j++) {
-                if (Integer.parseInt(inputNumbers[i]) == Integer.parseInt(inputNumbers[j])) {
-                    System.out.println("중복된 번호가 있습니다. 다시 입력해주세요.");
-                    System.out.println("1~45 사이의 숫자를 (,)로 구분하여 6개의 값을 입력해주세요.#3");
-                    return false;
-                }
-            }
+        if (sameCount == 6) {
+            return 1;
+        } else if (sameCount == 5 && isSameBonusNumber) {
+            return 2;
+        } else if (sameCount == 5) {
+            return 3;
+        } else if (sameCount == 4) {
+            return 4;
+        } else if (sameCount == 3) {
+            return 5;
         }
-        return true;
-    }
-
-    private static boolean isValidRange(String[] inputNumbers) {
-        for (String validNumber : inputNumbers) {
-            int value = Integer.parseInt(validNumber.trim());
-            if (value < 1 || value > 45) {
-                System.out.println("1~45 사이의 숫자를 (,)로 구분하여 6개의 값을 입력해주세요.#2");
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static boolean isValidInput(String[] inputNumbers) {
-        if (inputNumbers.length != INPUT_LENGTH) {
-            System.out.println("1~45 사이의 숫자를 (,)로 구분하여 6개의 값을 입력해주세요.#1");
-            return false;
-        }
-        return true;
+        return 6;
     }
 }
 
