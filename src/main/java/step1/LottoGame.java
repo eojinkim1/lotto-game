dc0b4ff0a1c39df33ac845475e183084aea49951
eojinkim1 <rladjwls999@gmail.com>
2024-05-03 23:04:04 +0900

    로또 게임 완성!

--- src/main/java/step1/LottoGame.java
+++ src/main/java/step1/LottoGame.java
@@ -74,19 +74,9 @@ public class LottoGame {
                 break;
             }
 
-            WinningNumber rank = new WinningNumber(winningLotto, bonusNumber);
+            WinningNumber winningNumber = new WinningNumber(winningLotto, bonusNumber);
+            winningNumber.printRank(lottoList);
 
-            int ranking = rank.rank(lottoList.get(0));
-            System.out.println(ranking);
-
-            System.out.println("당첨통계");
-            System.out.println("---");
-            System.out.println("3개 일치 (5,000원) - " + "변수" + "개");
-            System.out.println("4개 일치 (50,000원) - " + "변수" + "개");
-            System.out.println("5개 일치 (1,500,000원) - " + "변수" + "개");
-            System.out.println("5개 일치 (30,000,000원) - " + "변수" + "개");
-            System.out.println("6개 일치 (2,000,000,000원) - " + "변수" + "개");
-            System.out.println("총 수익률은 " + "" + "% 입니다.");
             break;
         }
     }
