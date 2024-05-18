96dbb2072edd99e3a7cd324d1d3029bd8ca4d7a2
eojinkim1 <rladjwls999@gmail.com>
2024-05-19 06:58:36 +0900

    코드 리팩토링

--- src/main/java/step_1/LottoGame.java
+++ src/main/java/step_1/LottoGame.java
@@ -4,31 +4,42 @@ import java.util.*;
 
 public class LottoGame {
     private static final Scanner SCANNER = new Scanner(System.in);
-    private static int PAYMENT = 0;
-    private static int PRICE = 1_000;
+    private static int MINIMUN_PAYMENT = 1_000;
+    private static int TICKET_PRICE = 1_000;
 
     public static void main(String[] args) {
         LottoStore lottoStore = new LottoStore();
 
+        int payment = getPayment();
+        int quantity = payment / TICKET_PRICE;
+        System.out.println(quantity + "개를 구매했습니다.");
+        List<Lotto> lottoList = lottoStore.sellLotto(payment);
+        for (Lotto lotto : lottoList) {
+            System.out.println(lotto);
+        }
+
+        Set<LottoNumber> winningNumbers = getWinningNumber();
+        LottoNumber bonusNumber = getBonusNumber(winningNumbers);
+
+        WinningNumber winningNumber = new WinningNumber(new Lotto(winningNumbers), bonusNumber);
+        winningNumber.printRank(lottoList);
+    }
+
+    private static int getPayment() {
         while (true) {
             System.out.println("구입 금액을 입력해 주세요.");
             try {
-                PAYMENT = Integer.parseInt(SCANNER.next());
-                if (PAYMENT < PRICE) {
+                int payment = Integer.parseInt(SCANNER.next());
+                if (payment < MINIMUN_PAYMENT) {
                     throw new IllegalArgumentException("1000원 이상의 값을 입력해주세요");
                 }
-                break;
+                return payment;
             } catch (IllegalArgumentException e) {
                 System.out.println("1000원 이상의 값을 입력해주세요");
             }
         }
-        int quantity = PAYMENT / PRICE;
-        System.out.println(quantity + "개를 구매했습니다.");
-        List<Lotto> lottoList = lottoStore.sellLotto(PAYMENT);
-        for (Lotto lotto : lottoList) {
-            System.out.println(lotto);
-        } 
-
+    }
+    private static Set<LottoNumber> getWinningNumber() {
         while (true) {
             System.out.println("당첨 번호를 입력해주세요.");
             String[] inputNumber;
@@ -40,47 +51,41 @@ public class LottoGame {
                     continue;
                 }
                 for (String input : inputNumber) {
-                    int number = Integer.parseInt(input);
-                    if (number < 1 || number > 45) {
-                        throw new IllegalArgumentException("1부터 45 사이의 숫자를 입력해주세요.");
-                    }
-                    LottoNumber lottoNumber = new LottoNumber(number);
-                    if (!winningNumbers.add(lottoNumber)) {
-                        throw new IllegalArgumentException("중복된 숫자를 입력했습니다. 중복 없이 다시 입력해주세요.");
-                    }
+                    LottoNumber lottoNumber = validateLottoNumber(input, winningNumbers);
                     winningNumbers.add(lottoNumber);
                 }
+                return winningNumbers;
             } catch (IllegalArgumentException e) {
                 System.out.println("1~45의 값을 중복되지 않도록 6개 입력해주세요");
-                continue;
             }
-            Lotto winningLotto = new Lotto(winningNumbers);
-
-            int inputBonusNumber;
-            LottoNumber bonusNumber;
-            while (true) {
-                System.out.println("보너스 번호를 입력하세요");
-                try {
-                    inputBonusNumber = Integer.parseInt(SCANNER.next());
-                    if (inputBonusNumber < 1 || inputBonusNumber > 45) {
-                        throw new IllegalArgumentException("1~45 사이의 숫자를 입력해주세요.");
-                    }
-                    bonusNumber = new LottoNumber(inputBonusNumber);
-                    if (winningNumbers.contains(bonusNumber)) {
-                        throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복되면 안 됩니다.");
-                    }
-                } catch (IllegalArgumentException e) {
-                    System.out.println("1~45 사이의 숫자를 당첨 번호와 중복되지 않게 입력해주세요.");
-                    continue;
-                }
-                break;
-            }
-
-            WinningNumber winningNumber = new WinningNumber(winningLotto, bonusNumber);
-            winningNumber.printRank(lottoList);
-
-            break;
         }
+    }
+    private static LottoNumber getBonusNumber(Set<LottoNumber> winningNumbers) {
+        while (true) {
+            System.out.println("보너스 번호를 입력하세요");
+            try {
+                String inputBonusNumber = SCANNER.next();
+                LottoNumber bonusNumber = validateLottoNumber(inputBonusNumber, null);
+                if (winningNumbers.contains(bonusNumber)) {
+                    throw new IllegalArgumentException("보너스 번호는 당첨 번호와 중복되면 안 됩니다.");
+                }
+                return bonusNumber;
+            } catch (IllegalArgumentException e) {
+                System.out.println("1~45 사이의 숫자를 당첨 번호와 중복되지 않게 입력해주세요.");
+            }
+        }
+    }
+
+    private static LottoNumber validateLottoNumber(String input, Set<LottoNumber> winningNumbers) {
+        int number = Integer.parseInt(input);
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("1부터 45 사이의 숫자를 입력해주세요.");
+        }
+        LottoNumber lottoNumber = new LottoNumber(number);
+        if (!winningNumbers.add(lottoNumber)) {
+            throw new IllegalArgumentException("중복된 숫자를 입력했습니다. 중복 없이 다시 입력해주세요.");
+        }
+        return lottoNumber;
     }
 }
 
